553634e0488c6d3a2623bf614c46348b00eb8440
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-03-31 20:16:31 +0530

    Added local high score capability
    Signed-off-by: yashovardhan99 <yashovardhandhanania@gmail.com>

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -6,13 +6,19 @@ using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
+    public static string HighScoreKey = "HIGHSCORE";
     public TextMeshProUGUI score;
     // Start is called before the first frame update
     void Start()
     {
         score.GetComponent<TextMeshProUGUI>().SetText("Your Score: "+ScoreHandler.score);
+        int highScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if(ScoreHandler.score > highScore) {
+            highScore = ScoreHandler.score;
+            PlayerPrefs.SetInt(HighScoreKey, highScore);
+            GetComponent<MenuManager>().updateHighScore();
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    public TextMeshProUGUI highScoreText;
+    void Start() {
+        updateHighScore();
+    }
+    public void updateHighScore() {
+        int highScore = PlayerPrefs.GetInt(GameOverManager.HighScoreKey, 0);
+        highScoreText.GetComponent<TextMeshProUGUI>().SetText("High Score: "+highScore);
+    }
     void Update() {
         if(Input.GetKeyDown(KeyCode.Space)) {
             startGame();
